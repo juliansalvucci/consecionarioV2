a61f21529b5851be038f96cb8c15b1425b6620e4
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-09-30 13:16:44 -0300

    refactor abmpais v0.1

--- Frontend/app/src/modules/pais/pais-lista/pais-lista.component.ts
+++ Frontend/app/src/modules/pais/pais-lista/pais-lista.component.ts
@@ -14,101 +14,96 @@ import { PaisComponent } from '../pais/pais.component';
   templateUrl: './pais-lista.component.html',
   styleUrls: ['./pais-lista.component.css'],
 })
-
-
 export class PaisListaComponent implements OnInit {
+  displayedColumns: string[] = ['nombrePais', 'acciones'];
+  dataSource!: MatTableDataSource<IGenerica>;
+
+  cargando: boolean = false;
+  filtro: string = '';
+
+  listaFiltro!: IGenerica[];
+  lista!: IGenerica[];
+
   constructor(
     public dialog: MatDialog,
     private service: PaisService,
     private dataService: DataService,
     public _MatPaginatorIntl: MatPaginatorIntl
-  ) 
-  {
+  ) {
     this.consultar();
-  }
-
-  ngOnInit(): void 
-  {
-    this._MatPaginatorIntl.itemsPerPageLabel = 'items por página';
   }
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-
-  
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  ngOnInit(): void {
+    this._MatPaginatorIntl.itemsPerPageLabel = 'items por página';
   }
-  
-  
+
   configTable() {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
   }
 
-  displayedColumns: string[] = ['nombrePais', 'acciones'];
-  dataSource!: MatTableDataSource<IGenerica>;
-
-  cargando: boolean = false;
-  lista!: IPais[];
-
   consultar(): void {
-    this.service.consulta().subscribe((r: IGenerica[]) => {
-      console.log(r);
-      this.lista = r;
-      this.dataSource = new MatTableDataSource(this.lista);
-      this.configTable();
+    try {
+      this.service.consulta().subscribe((r: IGenerica[]) => {
+        console.log(r);
+        this.lista = r;
+        this.dataSource = new MatTableDataSource(this.lista);
+        this.configTable();
+        this.cargando = false;
+      });
+    } catch (e) {
+      console.log(e);
       this.cargando = false;
-    });
+    }
   }
 
-  abrirModal(id:number): void {
+  abrirModal(id: number): void {
     this.dataService.id = id;
     const dialogRef = this.dialog.open(PaisComponent, {
       width: '450px',
     });
-    dialogRef.afterClosed().subscribe(result => {
-
-      if (this.dataService.object != null)
-      {
+    dialogRef.afterClosed().subscribe((result) => {
+      if (this.dataService.object != null) {
         this.lista = this.lista.filter((element) => element.id != id);
         this.lista.push(this.dataService.object);
         this.dataSource = new MatTableDataSource(this.lista);
         this.configTable();
-        this.dataService.object = null
+        this.dataService.object = null;
       }
       this.cargando = false;
     });
   }
-
 
   eliminar(id: number) {
-    this.service.baja(id).subscribe((r) => {
-      if(r){
-        this.lista = this.lista.filter(element => element.id != id)
-        this.dataSource = new MatTableDataSource(this.lista);
-        this.configTable();
-      }
+    try {
+      this.service.baja(id).subscribe((r) => {
+        if (r) {
+          //Si el back me devuelve un true.
+          this.lista = this.lista.filter((element) => element.id != id);
+          this.dataSource = new MatTableDataSource(this.lista);
+          this.configTable();
+        }
+        this.cargando = false;
+      });
+    } catch (e) {
+      console.log(e);
       this.cargando = false;
-    }, e => {
-      console.log(e)
-    });
+    }
   }
 
-  listaFiltro!: IGenerica[]
-  filtro: string = ''
-
-  filtrar()
-  {
-    if(this.filtro == "")
-    {
+  filtrar() {
+    if (this.filtro == '') {
       this.listaFiltro = this.lista;
-    } 
-    else
-    {
-      this.listaFiltro = this.lista?.filter(f => f.nombrePais?.toLowerCase().trim().includes(this.filtro.toLocaleLowerCase()));
+    } else {
+      this.listaFiltro = this.lista?.filter((f) =>
+        f.nombrePais
+          ?.toLowerCase()
+          .trim()
+          .includes(this.filtro.toLocaleLowerCase())
+      );
     }
     this.dataSource = new MatTableDataSource(this.listaFiltro);
     this.configTable();
@@ -117,6 +112,3 @@ export class PaisListaComponent implements OnInit {
 
 export interface IGenerica extends IPais{}
 
-//Datos útiles
-
-//this.lista = this.lista.map(objeto => objeto.id === id ? this.dataService.object : objeto)  Actualiza la lista solo para elementos modificados, pero no para nuevos, por eso se descarta.
