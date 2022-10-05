f0afaf6331ab9c51693b4180cfde9c9a2c2af09d
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-10-05 15:47:20 -0300

    venta class

---
+++ Backend/tppa/src/main/java/com/tppa/tppa/Models/Venta.java
@@ -0,0 +1,88 @@
+package com.tppa.tppa.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table (name="venta") 
+
+public class Venta 
+{
+    @Id @GeneratedValue(strategy=javax.persistence.GenerationType.AUTO)
+    private long id;
+    @NotNull
+    private String fechaVenta;
+    @NotNull
+    private Double costo;
+    @OneToOne
+    private Auto auto;
+    @OneToOne
+    private Cliente cliente;
+
+
+    public Cliente getCliente()
+    {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) 
+    {
+        this.cliente = cliente;
+    }
+
+    public long getId() 
+    {
+        return id;
+    }
+
+    public void setId(long id) 
+    {
+        this.id = id;
+    }
+    
+    public Auto getAuto() 
+    {
+        return auto;
+    }
+
+    public void setAuto(Auto auto) 
+    {
+        this.auto = auto;
+    }
+
+    public String getFechaVenta() 
+    {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(String fechaVenta) 
+    {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public Double getCosto() 
+    {
+        return costo;
+    }
+
+    public void setCosto(Double costo)
+    {
+        this.costo = costo;
+    }
+
+    ///////////////////////////////////////////
+    public Venta(){}
+
+    public Venta(long id, @NotNull String fechaVenta, Auto auto, Cliente cliente)
+    {
+        setId(id);
+        setFechaVenta(fechaVenta);
+        setAuto(auto);
+        setCliente(cliente);
+        setCosto(costo);
+    }  
+}
