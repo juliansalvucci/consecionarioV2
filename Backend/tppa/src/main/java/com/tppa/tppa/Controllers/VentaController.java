3a4dcc9229f40d95820bec283fa53168ebe877cf
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-11-06 11:12:56 -0300

    criterias en venta

--- Backend/tppa/src/main/java/com/tppa/tppa/Controllers/VentaController.java
+++ Backend/tppa/src/main/java/com/tppa/tppa/Controllers/VentaController.java
@@ -1,7 +1,14 @@
 package com.tppa.tppa.Controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,6 +29,11 @@ public class VentaController
 {
     @Autowired
     VentaService service;
+    EntityManager em;
+
+    public VentaController(EntityManager em){
+        this.em = em;
+    }
 
     @GetMapping()
     public ArrayList<Venta> obtener()
@@ -29,6 +41,50 @@ public class VentaController
         return service.obtener();
     }
 
+    //Endpoint para buscar ventas en un intervalo de costos.
+    @GetMapping(path = "/criteria")
+    public List<Venta> obtenerPorCriteria(Double montoInicial, Double montoFinal, String fechaDesde,String fechaHasta) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Venta> cr = cb.createQuery(Venta.class);
+        Root<Venta> root = cr.from(Venta.class);
+
+        cr.select(root).where(cb.between(root.get("costo"), montoInicial, montoFinal));
+
+        cr.select(root).where(cb.between(root.get("fechaVenta"), fechaDesde, fechaHasta));
+
+        TypedQuery<Venta> query = em.createQuery(cr);
+        List<Venta> results = query.getResultList();
+
+        return results;
+    }
+
+    @GetMapping(path = "/criteria1")
+    public List<Venta> obtenerPorCriteria1(Double montoInicial, Double montoFinal) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Venta> cr = cb.createQuery(Venta.class);
+        Root<Venta> root = cr.from(Venta.class);
+
+        cr.select(root).where(cb.between(root.get("costo"), montoInicial, montoFinal));
+
+        TypedQuery<Venta> query = em.createQuery(cr);
+        List<Venta> results = query.getResultList();
+
+        return results;
+    }
+
+    @GetMapping(path = "/criteria2")
+    public List<Venta> obtenerPorCriteri2(String fechaDesde,String fechaHasta) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Venta> cr = cb.createQuery(Venta.class);
+        Root<Venta> root = cr.from(Venta.class);
+
+        cr.select(root).where(cb.between(root.get("fechaVenta"), fechaDesde, fechaHasta));
+
+        TypedQuery<Venta> query = em.createQuery(cr);
+        List<Venta> results = query.getResultList();
+
+        return results;
+    }
 
     @PostMapping()
     public Venta guardar(@RequestBody Venta venta)
