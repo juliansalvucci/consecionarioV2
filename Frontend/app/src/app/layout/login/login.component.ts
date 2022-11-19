7dc9f7b3f147d685e067c094181f251621a83c8f
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-11-18 21:24:22 -0300

    logn front v0.1

--- Frontend/app/src/app/layout/login/login.component.ts
+++ Frontend/app/src/app/layout/login/login.component.ts
@@ -1,24 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { IUsuario } from 'src/interfaces/IUsuario';
+import { AuthService } from 'src/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private fb: FormBuilder,
+  ) {}
 
-  constructor(private router:Router ) { }
+  registerForm = this.fb.group({
+    nombreUsuario: ['', Validators.required],
+    contraseña: ['', Validators.required],
+  });
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public STR_SYSTEM = 'listaAutos';
+
+
+  login() {
+    try {
+      console.log(this.registerForm.value);
+      this.authService.login(this.registerForm.value).subscribe((data:IUsuario) => {
+        console.log('Registro realizado con éxito');
+        this.router.navigateByUrl('system');
+        console.log(data)
+        localStorage.setItem("user",JSON.stringify(data));
+        const usuario = localStorage.getItem("user");
+        if(usuario){
+          let jsonUser = JSON.parse(usuario);
+          console.log('Test', jsonUser.apellido)
+        } 
+      });
+    } catch (e) {
+      console.log(this.registerForm.value);
+      console.log('modelo invalido');
+    }
   }
-
-  public STR_SYSTEM = 'listaAutos'
-
-  login(){
-
-    this.router.navigateByUrl('system')
-    console.log('test')
-  }
-
 }
--- Frontend/app/src/services/auth/auth.service.ts
+++ Frontend/app/src/services/auth/auth.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(data:any):Observable<any>
+  {
+    return this.http.post('http://localhost:8080/Vendedor/login', data);
+  }
 }
