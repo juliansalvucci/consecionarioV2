ce07f470b481fef95344ff7a374c4ad772439edf
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-11-23 20:18:13 -0300

    inicio test

--- Backend/tppa/src/test/java/com/tppa/tppa/TppaApplicationTests.java
+++ Backend/tppa/src/test/java/com/tppa/tppa/TppaApplicationTests.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.tppa.tppa.Models.Auto;
+import com.tppa.tppa.Models.Categoria;
+import com.tppa.tppa.Models.Pais;
+import com.tppa.tppa.strategies.costoStrategies.EstrategiaCostoAmericaExtranjero;
+
 
 @SpringBootTest
 class TppaApplicationTests {
@@ -12,9 +17,21 @@ class TppaApplicationTests {
 	@Test
 	void contextLoads() {
 
-		var numero = 1;
+		EstrategiaCostoAmericaExtranjero ece = new EstrategiaCostoAmericaExtranjero();
+		Auto auto = new Auto();
+		Pais pais = new Pais();
+		Categoria categoria = new Categoria();
 
-		assertEquals(1, numero);
+		pais.setNombrePais("testPais");
+		categoria.setPorcentaje(10);
+		pais.setCategoria(categoria);
+		auto.setPais(pais);
+
+		auto.setPrecio(230.000);
+
+		var a = ece.calcularCosto(auto);
+        
+		assertEquals(23.000, a.getCosto());
 	}
 
 }
