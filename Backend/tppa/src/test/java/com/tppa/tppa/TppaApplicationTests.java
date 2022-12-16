d20a5197aed099cadc4af230ff6f784fcc66c2f2
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-12-15 23:32:46 -0300

    fix import

--- Backend/tppa/src/test/java/com/tppa/tppa/TppaApplicationTests.java
+++ Backend/tppa/src/test/java/com/tppa/tppa/TppaApplicationTests.java
@@ -2,7 +2,6 @@ package com.tppa.tppa;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
